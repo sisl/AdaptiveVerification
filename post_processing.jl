d9c6923103e6b44e8061e30b1f5c23b681e6af3e
Sydney Katz <sydney.m.katz@wustl.edu>
2020-08-18 08:59:28 -0700

    course discretization

--- post_processing.jl
+++ post_processing.jl
@@ -1,8 +1,12 @@
 hsplits = [150, 800, 1250, 4000, Inf]
 hwidths = [25, 50, 100, 250, 1000]
 
+course_hwidth = 1000
+
 ḣ₀splits = [30, 50, Inf]
 ḣ₀widths = [3, 5, 10]
+
+course_ḣ₀width = 8
 
 # ḣ₁splits = [0, 30, 50]
 # ḣ₁widths = [3, 5, 10]
@@ -49,6 +53,41 @@ function discretize!(curr_node::NODE)
     curr_diff = ubs .- lbs
     curr_diff[1] > hwidths[hbin] ? push!(dims_to_split, 1) : nothing
     curr_diff[2] > ḣ₀widths[ḣ₀bin] ? push!(dims_to_split, 2) : nothing
+
+    next_nodes = []
+
+    if length(dims_to_split) > 0
+        # println(curr_diff)
+        new_lbs, new_ubs = split_bounds(curr_node, dims_to_split = dims_to_split)
+        for i = 1:length(new_lbs)
+            push!(next_nodes, node(lbs = new_lbs[i], ubs = new_ubs[i], cats = curr_node.cats))
+        end
+        curr_node.children = next_nodes
+    end
+
+    return next_nodes
+end
+
+function discretize_for_mdp_course(root_node::NODE)
+    q = Queue{NODE}()
+    enqueue!(q, root_node)
+    while length(q) > 0
+        curr_node = dequeue!(q)
+        next_nodes = length(curr_node.children) > 0 ? curr_node.children : discretize_course!(curr_node)
+        for i = 1:length(next_nodes)
+            enqueue!(q, next_nodes[i])
+        end 
+    end
+end
+
+function discretize_course!(curr_node::NODE)
+    ubs = unnormalize_bounds(curr_node.ubs)
+    lbs = unnormalize_bounds(curr_node.lbs)
+
+    dims_to_split = []
+    curr_diff = ubs .- lbs
+    curr_diff[1] > course_hwidth ? push!(dims_to_split, 1) : nothing
+    curr_diff[2] > course_ḣ₀width ? push!(dims_to_split, 2) : nothing
 
     next_nodes = []
 
